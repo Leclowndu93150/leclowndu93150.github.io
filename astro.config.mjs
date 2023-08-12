b2b3722da63e8cb2d3db630fa170ac53a7719a9d
Leclowndu93150 <xdr.a123454321@gmail.com>
2023-08-12 15:28:21 +0200

    gag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://Leclowndu93150.github.io',
-    base: '/leclowndu93150.github.io',
+    site: 'https://Leclowndu93150.github.io'
 })
